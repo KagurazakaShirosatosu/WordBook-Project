f7aa292848f4e0ae7ad06746767fc8f28789f38a
Luobogao <servicer@live.cn>
2017-03-13 00:19:21 +0800

    更新对php7的滋次

--- add.php
+++ add.php
@@ -7,16 +7,14 @@ $note = $_POST["note"];
 $date = date('Y-m-d H:i:s');
 
 
-@$con = mysql_connect("localhost","word","word");
+@$con = mysqli_connect("localhost","word","word","word");
 if(!$con)
 {
-    die('与数据库建立连接失败,请联系luobogao修复数据库！1.可能是数据库掉线;2.可能是服务器预存密码错误');
+    die('数据库炸了');
 }
-mysql_select_db("word",$con)
-or die("数据库不存在");
 
 $sql_insert = "insert into word (add_time,word,mean,note) values('$date','$word','$mean','$note')";
-$res_insert = mysql_query($sql_insert);
+$res_insert = mysqli_query($con,$sql_insert);
 if($res_insert)
 {
 	echo "<script>alert('添加成功'); history.go(-1);</script>";
