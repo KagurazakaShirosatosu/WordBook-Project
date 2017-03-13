e385aeb254ec9bf0fbb879d371a0a180a8e24239
Luobogao <servicer@live.cn>
2017-03-14 01:34:04 +0900

    renew db

--- qwq123.sql
+++ qwq123.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: localhost
--- 產生時間： 2017 年 03 月 13 日 23:09
+-- 產生時間： 2017 年 03 月 14 日 00:27
 -- 伺服器版本: 5.5.48-log
 -- PHP 版本： 7.0.7
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- 資料庫： `word`
 --
-CREATE DATABASE IF NOT EXISTS `word` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `word`;
 
 -- --------------------------------------------------------
 
@@ -30,7 +28,7 @@ USE `word`;
 
 CREATE TABLE IF NOT EXISTS `classmate` (
   `id` smallint(6) NOT NULL,
-  `namae` tinytext NOT NULL,
+  `namae` varchar(15) NOT NULL,
   `passwd` tinytext NOT NULL,
   `reg_time` datetime NOT NULL,
   `reg_ip` tinytext NOT NULL,
@@ -44,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `classmate` (
 --
 
 INSERT INTO `classmate` (`id`, `namae`, `passwd`, `reg_time`, `reg_ip`, `lastlog_time`, `lastlog_ip`, `mail`) VALUES
-(0, 'Luobogao', 'qwq', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '', '');
+(0, 'Luobogao', 'qwq', '0000-00-00 00:00:00', '', '2017-03-14 00:24:40', '210.140.40.12', '');
 
 -- --------------------------------------------------------
 
@@ -53,7 +51,7 @@ INSERT INTO `classmate` (`id`, `namae`, `passwd`, `reg_time`, `reg_ip`, `lastlog
 --
 
 CREATE TABLE IF NOT EXISTS `word` (
-  `user_id` smallint(6) NOT NULL,
+  `namae` varchar(15) NOT NULL,
   `add_time` datetime NOT NULL,
   `word` text NOT NULL,
   `mean` text NOT NULL,
@@ -64,18 +62,28 @@ CREATE TABLE IF NOT EXISTS `word` (
 -- 資料表的匯出資料 `word`
 --
 
-INSERT INTO `word` (`user_id`, `add_time`, `word`, `mean`, `note`) VALUES
-(0, '2017-03-12 22:31:47', 'bi9swm', '萌萌星美', '写作萌萌星美读作萌老板');
+INSERT INTO `word` (`namae`, `add_time`, `word`, `mean`, `note`) VALUES
+('', '2017-03-12 22:31:47', 'bi9swm', '萌萌星美', '写作萌萌星美读作萌老板');
 
 --
 -- 已匯出資料表的索引
 --
 
 --
+-- 資料表索引 `classmate`
+--
+ALTER TABLE `classmate`
+  ADD PRIMARY KEY (`namae`),
+  ADD KEY `id` (`id`),
+  ADD KEY `id_2` (`id`);
+
+--
 -- 資料表索引 `word`
 --
 ALTER TABLE `word`
-  ADD UNIQUE KEY `add_time` (`add_time`);
+  ADD PRIMARY KEY (`namae`),
+  ADD UNIQUE KEY `add_time` (`add_time`),
+  ADD KEY `add_time_2` (`add_time`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
