bd4fab6b3525e91a997452c98fe75b8f79911625
Luobogao <servicer@live.cn>
2017-03-19 17:47:42 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
+<link rel="apple-touch-startup-image" href="images/9433274_p0.jpg">
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>You-On EDU 背单词计划</title>
 <meta name="ViewPort" content="initial-scale=1, minimum-scale=1, width=device-width">
